b2869b20ddc931fa99f8887da18d14b9743bb9b3
papasavvasn <papasavvasn@media-saturn.com>
2020-07-25 19:03:30 +0200

    feat(part3-express): ex 3.8 phonebook step8 complete part3a

--- index.ts
+++ index.ts
@@ -6,7 +6,16 @@ const app = express()
 
 // the use of json parser gives us access to request.body
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(function (tokens, req, res) {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        JSON.stringify(req.body)
+    ].join(' ')
+}))
 
 
 interface Person {
