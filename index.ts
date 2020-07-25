81e80ef797595e2be315a9111fdb7f5070fcbfb7
papasavvasn <papasavvasn@media-saturn.com>
2020-07-25 15:06:27 +0200

    feat(part3): complete exercice 3.3 phonebook step3

--- index.ts
+++ index.ts
@@ -7,22 +7,32 @@ const app = express()
 const persons = [{
     name: "Arto Hellas",
     number: "040-123456",
-    id: 1
+    id: "1"
 }, {
     name: "Ada Lovelace",
     number: "39-44-532353",
-    id: 2
+    id: "2"
 },
 {
     name: "Dam Abramov",
     number: "12-43-23423",
-    id: 3
+    id: "3"
 }
 ]
+
+app.get("/api/persons/:id", (req: Request, res: Response) => {
+    const person = persons.find(person => person.id === req.params.id)
+    if (person) {
+        res.json(person);
+    } else {
+        res.status(404).end()
+    }
+})
 
 app.get("/api/persons", (req: Request, res: Response) => {
     res.json(persons);
 })
+
 
 app.get("/info", (req: Request, res: Response) => {
     res.send(`PhoneBook has info for ${persons.length} people
